0d1842439c449076a0d92b412c25a59d59eb075d
Ivan Gozhyi <givan2981@gmail.com>
2025-07-22 12:39:48 +0300

    First Task

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+let str = "Hello World.";
+let num = 12;
+let bigIntNum = 1234567890123456789012345678901234567890n;
+let bool = true;
+let undef = undefined;
+let nul = null;
+let sym = Symbol("символ");
+let obj = {key: "значення"};
+let arr = [1, 2, 3];
+let func = function() {};
+
+console.log(`str = ${str} | тип: ${typeof str}`);
+console.log(`num = ${num} | тип: ${typeof num}`);
+console.log(`bigIntNum = ${bigIntNum} | тип: ${typeof bigIntNum}`);
+console.log(`bool = ${bool} | тип: ${typeof bool}`);
+console.log(`undef = ${undef} | тип: ${typeof undef}`);
+console.log(`nul = ${nul} | тип: ${typeof nul}`);
+console.log(`sym = ${sym.toString()} | тип: ${typeof sym}`);
+console.log(`obj = ${JSON.stringify(obj)} | тип: ${typeof obj}`);
+console.log(`arr = ${arr} | тип: ${typeof arr}`);
+console.log(`func = ${func} | тип: ${typeof func}`);
