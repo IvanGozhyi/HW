cd52cd8d8fc106e2a6a7e2d7947535b9f2b5192d
Ivan Gozhyi <givan2981@gmail.com>
2025-08-12 14:06:11 +0300

    Homework 5

---
+++ HW5/hw5.js
@@ -0,0 +1,90 @@
+let choice = parseInt(prompt("What is your choice?"));
+
+
+
+switch (choice) {
+    case 1:
+const sum = (function () {
+    let total = 0;
+    return function (num) {
+        total += num;
+        return total;
+    };
+})();
+
+        console.log(sum(10));
+        console.log(sum(9));
+        console.log(sum(68));
+        console.log(sum(41));
+break;
+
+case 2:
+    function curryMultiply (a) {
+        return function (b){
+            return a * b;
+        };
+    }
+
+    console.log(curryMultiply(9)(10));
+    break;
+
+
+case 3:
+
+//Recursive variant
+
+/*
+function askNumberRecursive(attempt = 1, lastInputR = null) {
+    let inputR = prompt(`Attempt ${attempt}: Type a number`);
+    let numR = Number(inputR);
+
+    lastInputR = numR;
+
+    if (numR > 100 || attempt >= 10 || Number.isNaN(numR)) {
+        console.log("Last input:", lastInputR);
+        return lastInputR;
+    }
+
+    return askNumberRecursive(attempt + 1, lastInputR);
+}
+
+    askNumberRecursive();
+
+
+ */
+
+
+        //Variant in cycle
+function askNumber() {
+    let lastInput;
+
+    for (let i = 0; i < 10; i++) {
+        let input = prompt("type the number:");
+        let num = Number(input);
+
+
+        lastInput = num;
+
+
+        if (num > 100) {
+            console.log("Congrats!")
+            break;
+        }
+
+        if (Number.isNaN(num))
+        {
+            console.log("This is not a number");
+            break;
+        }
+    }
+
+    console.log("Last input:", lastInput);
+}
+askNumber();
+    break;
+
+    default:
+        alert("Please enter a number 1-3");
+        break;
+}
+
