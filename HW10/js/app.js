29f1b558f75b047cc89c67d9f577d7202cc4ffbe
Ivan Gozhyi <givan2981@gmail.com>
2025-09-07 17:06:31 +0300

    HW11

--- HW10/js/app.js
+++ HW10/js/app.js
@@ -1,4 +1,21 @@
 
+const cities = {
+  ODS: "Odesa",
+  KH: "Kharkiv",
+  MK: "Mykolaiv",
+  KV: "Kyiv",
+  DP: "Dnipro",
+  LT: "Lutsk",
+  BT: "Bila Tserkva",
+  ZH: "Zhytomyr",
+  CH: "Cherkasy",
+  CHR: "Chernihiv"
+};
+
+
+
+
+
 const showDescription = product => {
   const parent = document.querySelector('.info');
   if (!parent) {
@@ -11,19 +28,86 @@ const showDescription = product => {
   const buyButton = document.createElement('button');
   buyButton.textContent = 'Buy';
   buyButton.addEventListener('click', () => {
-    const message = document.querySelector('.order-details');
-    const orderMessage = document.createElement('span');
-    orderMessage.innerText = "";
-    message.innerHTML = "";
-    orderMessage.textContent = 'The product was successfully ordered!';
-    const cancelButton = document.createElement('button');
-    cancelButton.textContent = 'X';
-    cancelButton.addEventListener('click', () => {
-      orderMessage.remove();
-      cancelButton.remove();
+    const form = document.forms[0];
+    const name = document.querySelector('.name');
+    const lName = document.querySelector('.lName');
+    const mName = form.querySelector('.mName');
+    const branchNumber = document.querySelector('.branchNumber');
+    const wayOfPayment = document.querySelector('.wayOfPayment');
+    const city = form.city.value;
+    const amount = document.querySelector('.amount');
+    const comment = document.querySelector('.comment');
+    const remember = document.querySelector('.rememberMe');
+    const subBtn = document.querySelector('.submit');
+    let orderInfo = [];
+    let rememberPerson = [];
+
+
+    form.classList.remove("order-form");
+    form.classList.add("visible");
+
+    const priceParent = document.querySelector('.forPrice');
+    const price = document.createElement("span");
+    priceParent.innerHTML = "";
+    priceParent.appendChild(price);
+
+    const updatePrice = () => {
+      const quantity = Number(amount.value) || 0;
+      price.textContent = `Price - ${product.price * quantity}$`;
+    };
+
+
+    updatePrice();
+
+    amount.addEventListener('input', updatePrice);
+
+    subBtn.addEventListener("click", () => {
+      orderInfo.push({
+        Name: name.value,
+        LastName: lName.value,
+        MidName: mName.value,
+        City: cities[city],
+        BranchNumber: branchNumber.value,
+        WayOfPayment: wayOfPayment.value,
+        Amount: amount.value,
+        Comment: comment.value,
+        Price: product.price * amount.value,
+      })
+      console.log(orderInfo);
+
+      if (remember.checked) {
+        rememberPerson.push({
+          Name: name.value,
+          LastName: lName.value,
+          MidName: mName.value,
+          City: cities[city],
+          BranchNumber: branchNumber.value,
+        })
+        console.log(rememberPerson);
+      }
+
+      if (!name.value.trim() || !lName.value.trim() || !mName.value.trim() || !branchNumber.value.trim() || !branchNumber.value.trim() ||! amount.value.trim()||! comment.value.trim() ) {
+        alert("Please fill all fields");
+      }else{
+        const message = document.querySelector('.order-details');
+        const orderMessage = document.createElement('span');
+        const cancelButton = document.createElement('button');
+        cancelButton.textContent = 'X';
+        orderMessage.innerText = "";
+        message.innerHTML = "";
+        orderMessage.textContent = 'The product was successfully ordered!';
+        cancelButton.addEventListener('click', () => {
+          orderMessage.remove();
+          cancelButton.remove();
+        })
+        message.appendChild(orderMessage);
+        message.appendChild(cancelButton);
+      }
+
+
     })
-    message.appendChild(orderMessage);
-    message.appendChild(cancelButton);
+
+
 
 
   })
@@ -118,3 +202,7 @@ const showProductsByCategory = category => {
 
 
 showCategories();
+
+
+
+
---
+++ HW11/hw.js
@@ -0,0 +1,56 @@
+
+
+let cities = {
+    KV : "Kyiv",
+    CHG: "Chicago",
+    BRL: "Berlin",
+    TKY: "Tokyo",
+};
+
+
+
+
+
+let submit = () => {
+    const form = document.forms[0];
+    const submitBtn = document.querySelector(".submit");
+    const fname = document.querySelector(".firstName");
+    const lname = document.querySelector(".lastName");
+    const date = document.querySelector(".date");
+    const gender = document.querySelector(".gender");
+    const address = document.querySelector(".address");
+    const city = form.city.value;
+
+   let showSkills = () =>{
+       const skills = [];
+
+       for(let i = 0; i < form.forLanguage.length; i++){
+           if(form.forLanguage[i].checked){
+               skills.push(form.forLanguage[i].value);
+           }
+       }
+       return skills;
+   }
+
+
+
+
+    submitBtn.addEventListener("click", () => {
+        document.write(`
+           <p>First Name: ${fname.value}</p>
+           <p>Last Name: ${lname.value}</p>
+           <p>B-Day: ${date.value}</p>
+           <p>Gender: ${gender.value}</p>
+           <p>Cities: ${cities[city]}</p>
+           <p>Address: ${address.value}</p>
+           <p>Programming languages: ${showSkills()}</p>
+        
+          
+`);
+
+
+
+    })
+}
+
+submit();
